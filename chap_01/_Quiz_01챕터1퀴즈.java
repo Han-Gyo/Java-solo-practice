e5ece0a3b280a066dd028b38f1a74f1072ef1f3f
Han-Gyo <tjwjdxo3940@naver.com>
2025-01-03 20:58:58 +0900

    first commit
    처음 올려보는 자바 챕터1 연습본 입니다.

---
+++ chap_01/_Quiz_01챕터1퀴즈.java
@@ -0,0 +1,25 @@
+package chap_01;
+
+public class _Quiz_01챕터1퀴즈 {
+    public static void main(String[] args) {
+       /* // 퀴즈 결과 = 아래
+        상암08번 버스
+        약 3분 후 도착
+        남은 거리 1.2km*/
+
+        // 버스 번호는 (1234,상암08)
+        String busno="상암08";
+        System.out.println(busno+"번 버스");
+
+        //남은 시간은 분 단위 (3분,5분)
+        int min = 3;
+        System.out.println("약 "+min+"분 후 도착");
+
+        // 남은 거리는 km 단위 ( 1.5km, 0.8km )
+        double distance = 1.2;
+        System.out.println("남은 거리 "+distance+"km");
+
+
+        /*영상 도움없이 이미 했던 자료들을 보면서 성공했다. 만족한다ㅎㅋㅎㅋ. */
+    }
+}
