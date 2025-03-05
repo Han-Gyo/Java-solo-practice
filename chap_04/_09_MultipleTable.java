4aebe66db9b7f6bc78f965d52751029c5c7a2227
Han-Gyo <tjwjdxo3940@naver.com>
2025-03-05 20:27:00 +0900

    챕터 4 기초 공부

---
+++ chap_04/_09_MultipleTable.java
@@ -0,0 +1,13 @@
+package chap_04;
+
+public class _09_MultipleTable {
+    public static void main(String[] args) {
+        //구구단
+        // 2 * 1 = 2
+        // 2 * 2 = 4
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+    }
+}
